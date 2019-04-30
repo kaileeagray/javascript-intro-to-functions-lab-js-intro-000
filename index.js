66f05b66664a815c091d6380ae73c9f87c509084
Kailee Gray <kaileeagray@gmail.com>
2019-04-30 17:15:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ function sayHiToGrandma(string) {
   } else if (string === string.toLowerCase()) {
     return "I can't hear you!";
   } else if (string === "I love you, Grandma.") {
+    return "I can't hear you!";
 
   }
   return "I can't hear you!";
